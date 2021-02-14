0c423b0fec6130e24d6578d7a4a15a13404785c9
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-02-14 21:25:59 +0530

    added embed with ocr

--- screenshot.py
+++ screenshot.py
@@ -4,16 +4,37 @@
 import discord
 from discord.ext import commands, tasks
 import asyncio
-from PIL import ImageGrab
+from PIL import Image, ImageEnhance, ImageFilter, ImageGrab
 from io import BytesIO
 import keyboard
+from datetime import datetime
+import pytz
+import numpy as np
+import pytesseract
 
 client = discord.Client()
-TOKEN = "ODA5NjQzMTEwNDg0MTQ4MjI0.YCYE5w.r52DUZ3qjOChZUY_Avz5AmH_QH8"
+TOKEN = ""
+screenshot_count = 0
 
 
 def take_screenshot():
-    return ImageGrab.grab()
+    left = 503
+    top = 390
+    right = 1617
+    bottom = 1079
+    image = ImageGrab.grab().convert("RGB")
+    im = image.crop((left, top, right, bottom))
+    na = np.array(im)
+    yellowY, yellowX = np.where(np.all(na == [222, 242, 248], axis=2))
+    top, bottom = yellowY[0], yellowY[-1]
+    left, right = yellowX[0], yellowX[-1]
+    ROI = na[top:bottom, left:right]
+    return Image.fromarray(ROI)
+
+
+def pytesseract_ocr(image):
+    text = pytesseract.image_to_string(image, config="--psm 6 --oem 3")
+    return text
 
 
 @client.event
@@ -27,12 +48,19 @@ async def on_ready():
 
 @tasks.loop(seconds=0.2)
 async def screenshot_takeNsend():
-    channel = client.get_channel(809642981753225256)
-    if keyboard.is_pressed("ctrl + a"):
+    channel = client.get_channel()
+    if keyboard.is_pressed("ctrl + b"):
+        global screenshot_count
+        screenshot_count += 1
         with BytesIO() as image_binary:
-            take_screenshot().save(image_binary, "PNG")
-            image_binary.seek(0)
-            await channel.send(file=discord.File(fp=image_binary, filename="image.png"))
+
+            screenshot_image = take_screenshot()
+            screenshot_image.saFritzingtime.now(pytz.timezone("Asia/Kolkata")),
+            )
+            embed.set_image(url="attachment://image.png")
+
+            # await channel.send(file=file)
+            await channel.send(file=file, embed=embed)
         print("Screenshot sent")
         await asyncio.sleep(1)
 
