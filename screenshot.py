999b7afcde94607c7754bc20217e5f1eaf3775fb
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-02-15 12:28:07 +0530

    added env variable

--- screenshot.py
+++ screenshot.py
@@ -11,34 +11,46 @@ from datetime import datetime
 import pytz
 import numpy as np
 import pytesseract
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 client = discord.Client()
-TOKEN = ""
+TOKEN = os.environ["TOKEN"]
+CHANNEL_ID = int(os.environ["ID"])
 screenshot_count = 0
 
 
 def take_screenshot():
-    left = 503
-    top = 390
+    left = 513
+    top = 349
     right = 1617
     bottom = 1079
-    image = ImageGrab.grab().convert("RGB")
-    im = image.crop((left, top, right, bottom))
+    ss_image = ImageGrab.grab().convert("RGB")
+    im = ss_image.crop((left, top, right, bottom))
     na = np.array(im)
     lavenderY, lavenderX = np.where(np.all(na == [222, 242, 248], axis=2))
     top, bottom = lavenderY[0], lavenderY[-1]
     left, right = lavenderX[0], lavenderX[-1]
     ROI = na[top:bottom, left:right]
-    return Image.fromarray(ROI)
+    cropped_image = Image.fromarray(ROI)
+    return cropped_image
 
 
 def pytesseract_ocr(image):
-    na = np.array(image)
-    whitesmokeY, _ = np.where(np.all(na == [237, 244, 246], axis=2))
-    top, bottom = whitesmokeY[0], whitesmokeY[-1]
-    ROI = na[: top - 40, :]
-    final = Image.fromarray(ROI)
-    text = pytesseract.image_to_string(final, config="--psm 6 --oem 3")
+    # If only question is needed
+    # na = np.array(image)
+    # whitesmokeY, _ = np.where(np.all(na == [237, 244, 246], axis=2))
+    # # if whitesmokeY.size == 0:
+    # #     greyY, _ = np.where(np.all(na == [233, 236, 239], axis=2))
+    # #     topgrey = greyY[0]
+    # #     ROI = na[: topgrey - 40, :]
+    # # else:
+    # topwhitesmoke = whitesmokeY[0]
+    # ROI = na[: topwhitesmoke - 40, :]
+    # final = Image.fromarray(ROI)
+    text = pytesseract.image_to_string(image, config="--psm 6 --oem 3")
     return text
 
 
@@ -53,7 +65,7 @@ async def on_ready():
 
 @tasks.loop(seconds=0.2)
 async def screenshot_takeNsend():
-    channel = client.get_channel()
+    channel = client.get_channel(CHANNEL_ID)
     if keyboard.is_pressed("ctrl + b"):
         global screenshot_count
         screenshot_count += 1
@@ -63,16 +75,17 @@ async def screenshot_takeNsend():
             screenshot_image.save(image_binary, "PNG")
             image_binary.seek(0)
             file = discord.File(fp=image_binary, filename="image.png")
-
             ocr_text = pytesseract_ocr(screenshot_image)
             embed = discord.Embed(
                 title=f"Screenshot {screenshot_count}",
                 colour=discord.Colour(0xD8C538),
-                description=pytesseract_ocr(screenshot_image),
+                description=ocr_text,
                 timestamp=datetime.now(pytz.timezone("Asia/Kolkata")),
             )
             embed.set_image(url="attachment://image.png")
             await channel.send(file=file, embed=embed)
+            await asyncio.sleep(1)
+            await channel.send("** **")
         print("Screenshot sent")
         await asyncio.sleep(1)
 
